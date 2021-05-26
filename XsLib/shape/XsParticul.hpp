44aa308a90844e217b4d2df76646029c38886269
Tyson <65432197+TYSON-Alii@users.noreply.github.com>
2021-05-26 18:43:34 +0300

    Add files via upload

---
+++ XsLib/shape/XsParticul.hpp
@@ -0,0 +1,79 @@
+class XsParticul {
+private:
+	std::deque<vex3f> __pos;
+	XsClock __tempTime, __create;
+	std::deque<XsClock> __time;
+public:
+	float lifeTime;
+	float reCreate;
+	XsVertexDepo vertices;
+	vex3f color;
+	vex3f pos;
+	int density;
+	vex3f random;
+	void draw(GLenum mode) {
+		if (__create.getMilliSeconds() > reCreate) {
+			__create.start();
+			__tempTime.start();
+			for (int __i = 0; __i < density; __i++) {
+				__time.push_back(__tempTime);
+				__pos.push_back(pos + (XsRand3f(random.x, random.y) / random.z));
+			}
+		}
+			glLoadIdentity();
+		for (int _i = 0; _i < __pos.size(); _i++) {
+			glColor3f(color.x, color.y, color.z);
+			glBegin(mode);
+			XsPrintVertex(vertices, XS_VERTEX_AND_NORMAL, __pos[_i]);
+			glEnd();
+			if (__time[_i].getMilliSeconds() > lifeTime) {
+				__time.pop_front();
+				__pos.pop_front();
+			}
+		}
+	}
+};
+class XsParticulX {
+private:
+	std::deque<vex3f> __pos;
+	XsClock __tempTime, __create;
+	std::deque<XsClock> __time;
+	std::deque<vex3f> __vel;
+	std::deque<vex3f> __color;
+public:
+	vex3f velocity;
+	float lifeTime;
+	float reCreate;
+	vex3f color;
+	vex3f pos;
+	int density;
+	vex3f posRandom;
+	vex3f colorRandom;
+	void draw(XsVertexDepo vertices, GLenum mode) {
+		if (__create.getMilliSeconds() > reCreate) {
+			__create.start();
+			for (int __i = 0; __i < density; __i++) {
+				__tempTime.start();
+				__time.push_back(__tempTime);
+				__pos.push_back(pos + (XsRand3f(posRandom.x, posRandom.y) / posRandom.z));
+				__vel.push_back(velocity);
+				__color.push_back(color + (XsRand3f(colorRandom.x, colorRandom.y) / colorRandom.z));
+			};
+		}
+		glLoadIdentity();
+		for (int _i = 0; _i < __pos.size(); _i++) {
+			__vel[_i] *= vex3f(1.01f);
+			__pos[_i] += __vel[_i];
+			glColor3f(__color[_i].x, __color[_i].y, __color[_i].z);
+			glBegin(mode);
+			XsPrintVertex(vertices, XS_VERTEX_AND_NORMAL, vex3f(1),__pos[_i]);
+			glEnd();
+			if (__time[_i].getMilliSeconds() > lifeTime) {
+				__time.pop_front();
+				__pos.pop_front();
+				__vel.pop_front();
+				__color.pop_front();
+			}
+		}
+	}
+};
